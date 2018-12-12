e73c7443ec3a0231cdb91d3f6d223783d2ff1614
IvanWang <56157187@qq.com>
2018-12-12 23:10:27 +0800

    修改图片上传文件大小限制：不大于30M

--- pages/order/pay/utils/config.js
+++ pages/order/pay/utils/config.js
@@ -3,7 +3,7 @@ var config = {
   //aliyun OSS config
   uploadImageUrl: `${fileHost}`, // 默认存在根目录，可根据需求改
   AccessKeySecret: '6FDB5fftlXxEyZ54obDjpXvtDwHrvC',        // AccessKeySecret 去你的阿里云上控制台上找
-     OSSAccessKeyId: 'LTAIpL2pPGEy5wFh',         // AccessKeyId 去你的阿里云上控制台上找
+  OSSAccessKeyId: 'LTAIpL2pPGEy5wFh',         // AccessKeyId 去你的阿里云上控制台上找
   timeout: 87600 //这个是上传文件时Policy的失效时间
 };
 module.exports = config
